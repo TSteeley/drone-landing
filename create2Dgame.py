a1687d1b2b8bcac6c872d91271b9047d77279dc0
TSteeley <thomassteele88@gmail.com>
2023-06-28 11:52:58 +1000

    Added target

--- create2Dgame.py
+++ create2Dgame.py
@@ -3,12 +3,14 @@ from brains import KeyBoard
 from gameState import gameState
 
 
-def pyGame2D(player: KeyBoard, state):
+def pyGame2D(player: KeyBoard):
     # Initialise pygame environment
     pygame.init()
     screen = pygame.display.set_mode((1280,720))
     clock = pygame.time.Clock()
     pygame.display.set_caption("AI drone")
+    print(pygame.display.get_surface())
+    state = gameState(screen)
     
     player.set_position(pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2))
     
@@ -27,14 +29,18 @@ def pyGame2D(player: KeyBoard, state):
         
         player.physics_update(state.dt)
         
+        state.draw(screen)
+        
+        
+        
         pygame.display.flip()
 
-        print(player.vel, player.engines.get_Throttle())
+        print(player.pos, player.engines.get_Throttle())
         
         state.set_dt(clock.tick(60) / 500)
 
     pygame.quit()
 
 if __name__ == '__main__':
-    pyGame2D(KeyBoard(), gameState())
+    pyGame2D(KeyBoard())
     
--- gameState.py
+++ gameState.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 class gameState:
     """ # Game State
@@ -6,12 +7,20 @@ class gameState:
     Carries important information about the game state
     """
     
-    def __init__(self):
+    def __init__(self, screen):
         self.dt = 0
+        self.points
         self.running = True
-        self.target = []
-        self.targetImage = pygame.transform.scale(
-            pygame.image.load("red_dot.png"), (200,200))
+        self.screenSize = screen.get_size()
+        self.set_randomTargetPos()
+        self.targetImage = pygame.transform.scale(pygame.image.load("red_dot.png"), (200,200))
         
     def set_dt(self, dt):
         self.dt = dt
+        
+    def draw(self, screen):
+        print(self.targetPos)
+        screen.blit(self.targetImage, self.targetPos)
+        
+    def set_randomTargetPos(self):
+        self.targetPos = (random.uniform(0,self.screenSize[0])//1,random.uniform(0,self.screenSize[1])//1)
